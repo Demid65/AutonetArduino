ec1300796e5afb394160041d118703c71ac4f9b4
Demid65 <Demid65e@yandex.ru>
2020-10-24 12:41:26 +0300

    Autoformat + Stepmotor Removed
    Stepmotor support removed.
    Code is autoformated

--- AutonetArduino.ino
+++ AutonetArduino.ino
@@ -5,27 +5,26 @@ char inChar;
 String RegA, RegB, RegC;
 int readMode = 0, ColorId, DriveMode = 0;
 Multiservo srv[12];
-Ultrasonic USonic(11,12);
+Ultrasonic USonic(11, 12);
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   for (int i = 0; i < 12; i++) {
-    
+
     srv[i].attach(i);
- 
+
   }
- pinMode(49,INPUT);
- pinMode(50,OUTPUT);
- pinMode(51,OUTPUT);
- pinMode(52,OUTPUT);
- pinMode(53,OUTPUT);
- pinMode(13,OUTPUT);
- pinMode(2,OUTPUT);
- digitalWrite(2,LOW);
-  pinMode(8,OUTPUT);
-pinMode(7,OUTPUT);
-pinMode(6,OUTPUT);
-pinMode(5,OUTPUT);
+  pinMode(49, INPUT);
+  pinMode(50, OUTPUT);
+  pinMode(51, OUTPUT);
+  pinMode(52, OUTPUT);
+  pinMode(53, OUTPUT);
+  pinMode(13, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
 }
 
 void loop() {
@@ -78,10 +77,10 @@ void ProcessIn1(char inCh) {
 }
 
 int GetHue() {
-  digitalWrite(13,HIGH);
+  digitalWrite(13, HIGH);
   delay(30);
-  digitalWrite(50,HIGH);
-  digitalWrite(51,LOW);
+  digitalWrite(50, HIGH);
+  digitalWrite(51, LOW);
   digitalWrite(52, LOW);
   digitalWrite(53, LOW);
   int R = 255 - pulseIn(49, LOW);
@@ -104,20 +103,20 @@ int GetHue() {
   } else if (V == B) {
     H = 60 * (R - G) / (V - M) + 240;
   }
-  digitalWrite(13,LOW);
+  digitalWrite(13, LOW);
   return H;
 }
 
-int getBrightness(){
-   digitalWrite(13,HIGH);
+int getBrightness() {
+  digitalWrite(13, HIGH);
   delay(30);
-  digitalWrite(50,HIGH);
-  digitalWrite(51,LOW);
+  digitalWrite(50, HIGH);
+  digitalWrite(51, LOW);
   digitalWrite(52, HIGH);
   digitalWrite(53, LOW);
 
-   return 255 - pulseIn(49, LOW);
-   digitalWrite(13,LOW);
+  return 255 - pulseIn(49, LOW);
+  digitalWrite(13, LOW);
 }
 
 void Execute(String A, String B, String C) {
@@ -125,9 +124,9 @@ void Execute(String A, String B, String C) {
     Serial.println("OK");
   } else if (C == "SetDM") {
     DriveMode = A.toInt();
-  } else if(C == "GetDM"){
+  } else if (C == "GetDM") {
     Serial.println(DriveMode);
-  }else if (C == "PinMode") {
+  } else if (C == "PinMode") {
     pinMode(B.toInt(), A.toInt());
   } else if (C == "DigitalWrite") {
     digitalWrite(B.toInt(), A.toInt());
@@ -140,151 +139,9 @@ void Execute(String A, String B, String C) {
   } else if (C == "SetServo") {
     srv[B.toInt()].write(A.toInt());
   } else if (C == "GetHue") {
-    Serial.println(GetHue());   
+    Serial.println(GetHue());
   } else if (C == "GetSonic") {
     Serial.println(USonic.read());
-  } else if (C == "StepMotor"){
-    StepMotor(A.toInt());
   } else
     Serial.println("Unknown Command " + C);
-}
-
-void StepMotor(int steps){
-  //int steps = degree/7;
-  if (steps>0){
-  for(int i=0;i<steps;i++)
-  motStep(); 
-  } else {
-  steps = -steps;  
-  for(int i=0;i<steps;i++)
-  antiStep(); 
-  }
-  
-}
-
-
-void motStep() {
- digitalWrite(8,1);
- digitalWrite(7,0);
- digitalWrite(6,1);
- digitalWrite(5,0);
- delay(10);
- digitalWrite(8,0);
- digitalWrite(7,1);
- digitalWrite(6,1);
- digitalWrite(5,0);
- delay(10);
- digitalWrite(8,0);
- digitalWrite(7,1);
- digitalWrite(6,0);
- digitalWrite(5,1);
- delay(10);
- digitalWrite(8,1);
- digitalWrite(7,0);
- digitalWrite(6,0);
- digitalWrite(5,1);
- delay(10);
- digitalWrite(8,1);
- digitalWrite(7,0);
- digitalWrite(6,1);
- digitalWrite(5,0);
-/* digitalWrite(8,1);
- digitalWrite(7,0);
- digitalWrite(6,0);
- digitalWrite(5,0);
- delay(10);
- digitalWrite(8,1);
- digitalWrite(7,0);
- digitalWrite(6,1);
- digitalWrite(5,0);
- delay(10);
- 
- digitalWrite(8,0);
- digitalWrite(7,0);
- digitalWrite(6,1);
- digitalWrite(5,0);
- delay(10);
- digitalWrite(8,0);
- digitalWrite(7,1);
- digitalWrite(6,1);
- digitalWrite(5,0);
- delay(10);
-
- digitalWrite(8,0);
- digitalWrite(7,1);
- digitalWrite(6,0);
- digitalWrite(5,0);
- delay(10);
- digitalWrite(8,0);
- digitalWrite(7,1);
- digitalWrite(6,0);
- digitalWrite(5,1);
- delay(10);
- 
- digitalWrite(8,0);
- digitalWrite(7,0);
- digitalWrite(6,0);
- digitalWrite(5,1);
- delay(10);
- digitalWrite(8,1);
- digitalWrite(7,0);
- digitalWrite(6,0);
- digitalWrite(5,1);
- delay(10);
-
- digitalWrite(8,0);
- digitalWrite(7,0);
- digitalWrite(6,0);
- digitalWrite(5,0);*/
-}
-
-void antiStep() {
- digitalWrite(8,1);
- digitalWrite(7,0);
- digitalWrite(6,0);
- digitalWrite(5,1);
- delay(10);
- digitalWrite(8,0);
- digitalWrite(7,0);
- digitalWrite(6,0);
- digitalWrite(5,1);
- delay(10);
- 
- digitalWrite(8,0);
- digitalWrite(7,1);
- digitalWrite(6,0);
- digitalWrite(5,1);
- delay(10);
-  digitalWrite(8,0);
- digitalWrite(7,1);
- digitalWrite(6,0);
- digitalWrite(5,0);
- delay(10);
-
- digitalWrite(8,0);
- digitalWrite(7,1);
- digitalWrite(6,1);
- digitalWrite(5,0);
- delay(10);
- digitalWrite(8,0);
- digitalWrite(7,0);
- digitalWrite(6,1);
- digitalWrite(5,0);
- delay(10);
-
- digitalWrite(8,1);
- digitalWrite(7,0);
- digitalWrite(6,1);
- digitalWrite(5,0);
- delay(10);
-  digitalWrite(8,1);
- digitalWrite(7,0);
- digitalWrite(6,0);
- digitalWrite(5,0);
- delay(10);
-
-  digitalWrite(8,0);
- digitalWrite(7,0);
- digitalWrite(6,0);
- digitalWrite(5,0);
 }
